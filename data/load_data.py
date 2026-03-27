77a11bc7fe6437e45a6d04d8618b8a0e5e03c5d5
jmsellitti1 <jmsellitti1@gmail.com>
2026-03-26 21:01:17 -0400

    Sample data generation

--- data/load_data.py
+++ data/load_data.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 data = statcast('2025-05-01')
 data.to_csv('statcast_data.csv', index=False)
-player_id = data['batter'].unique()[0]
-batting_stats = statcast_batter('2025-01-01', '2025-12-31', player_id)
-batting_stats.to_csv(f'{player_id}_batting_stats.csv', index=False)
+
+for i in range(10):
+    player_id = data['batter'].unique()[i]
+    batting_stats = statcast_batter('2025-01-01', '2025-12-31', player_id)
+    batting_stats.to_csv(f'playerStats/{player_id}_batting_stats.csv', index=False)
